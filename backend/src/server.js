6ae5e4a78dc72388ec87c7620982a6ca7d6ad220
vikas kumar <vk8756008@gmail.com>
2025-08-08 01:59:12 +0530

    project completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
 app.use(
@@ -18,8 +20,16 @@ app.use(
 app.use(express.json());
 app.use(rateLimiter);
 
-
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server started on port:", PORT);
